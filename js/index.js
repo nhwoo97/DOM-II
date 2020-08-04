849bcb17e91d9f78e49be4015f86c3e0393e05c5
Namhyuk Woo <namhyukwoo@Namhyuks-MacBook-Air.local>
2020-08-04 13:36:53 -0700

    Added a couple of event listeners (3)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+//fun bus logo
+const funBusLogo = document.querySelector('.logo-heading')
+
+funBusLogo.addEventListener('mouseenter', () => {
+    funBusLogo.style.transform = 'scale(1.5)'
+    funBusLogo.style.transition = 'all 0.3s'
+})
+
+funBusLogo.addEventListener('mouseleave', () => {
+    funBusLogo.style.transform = 'scale(1.0)'
+})
+
+//nav bar
+const navBar = document.querySelectorAll('.nav-link')
+navBar.forEach(element => {
+    element.addEventListener('click', () => {
+        if(element.style.color === 'red'){
+            element.style.color = 'black'
+            element.style.fontStyle = 'normal'
+        }else{
+        element.style.color = 'red'
+        element.style.fontStyle = 'italic'
+    }
+    })
+})
+
+//
